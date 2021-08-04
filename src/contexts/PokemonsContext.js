c0a857f95e8706262e8f4fb4d44f0bb9da8f7456
Vitor Lourenco <vitor.registros@lourencos.net>
2021-08-04 19:26:33 -0300

    Fix dependency array for react hook

--- src/contexts/PokemonsContext.js
+++ src/contexts/PokemonsContext.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext, useContext } from "react";
+import { useEffect, useState, createContext, useContext, useCallback } from "react";
 import axios from "axios";
 
 import UserContext from "../contexts/UserContext";
@@ -10,11 +10,7 @@ export function PokemonsProvider({ children }) {
     const { token } = useContext(UserContext);
     const [pokemons, setPokemons] = useState(null);
 
-    useEffect(() => {
-        updatePokemons();
-    }, [token?.token]);
-
-    function updatePokemons() {
+    const updatePokemons = useCallback(()=> {
         axios.get(`${process.env.REACT_APP_API_BASE_URL}/pokemons`, {
             headers: {
                 Authorization: `Bearer ${token?.token}`
@@ -22,7 +18,11 @@ export function PokemonsProvider({ children }) {
         }).then(response => {
             setPokemons(response.data);
         });
-    }
+    },[token?.token]);
+
+    useEffect(() => {
+        updatePokemons();
+    }, [token?.token, updatePokemons]);
 
     return (
         <PokemonsContext.Provider value={{ pokemons, updatePokemons }}>
